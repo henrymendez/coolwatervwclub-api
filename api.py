ba03e87e037125cef0460a5fa7a4d5a85c8daaf4
Henry Mendez <henry95@gmail.com>
2019-01-27 18:10:41 -0500

    #2: Not returning blob urls that end with a /

--- api.py
+++ api.py
@@ -1,23 +1,26 @@
-import sys
+import glob
 
 from flask import jsonify
-from google.auth import compute_engine
 
+from google.auth import compute_engine
 from google.cloud import storage
 
 
 def get_urls(bucket_name,prefix=None):
     """Lists all the blobs in the bucket."""
+    # Local debugging. Uncomment below line, comment following 2
     #storage_client = storage.Client.from_service_account_json('key/coolwatervwclub-read-only.json')
     credentials = compute_engine.Credentials()
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.get_bucket(bucket_name)
 
-    blobs = bucket.list_blobs(prefix=prefix,delimiter='foo')
-    #print("blobs: ", blobs, file=sys.stdout)
+    blobs = bucket.list_blobs(prefix=prefix,delimiter=None)
 
     blob_list = []
     for blob in blobs:
+        # Don't return urls that end in /
+        if blob.public_url.endswith('/'):
+            continue
         item = { 'url': blob.public_url }
         blob_list.append(item)
 
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 #
-import sys
 
 from flask import Flask
 from flask import abort, jsonify, make_response
 
-from google.cloud import storage
-
 from api import get_urls
 
+# Globals
+BUCKET='coolwatervwclub'
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -18,10 +17,11 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 @app.route("/<path:prefix>")
 def root(prefix):
 
-    urls = get_urls('coolwatervwclub',prefix)
+    urls = get_urls(BUCKET, prefix)
 
     if urls == None:
         abort(404)
